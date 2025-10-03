f1be8a8296b3c9926c49e6965b312897866d940a
gcoogan1 <gcoogan1@gmail.com>
2025-10-03 14:50:57 -0400

    Add vite-plugin-svgr package and configure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
+import Menu from "./assets/icon/menu.svg?react";
 function App() {
 
   return (
-    <>
+    <div>
       <h1>Hello Vite + React!</h1>
-    </>
+      <Menu color="red" />
+    </div>
   )
 }
 

6b40146ae7de8e12e515626e538ae6e673c4e3c9
gcoogan1 <gcoogan1@gmail.com>
2025-10-03 14:17:20 -0400

    Remove default styling. Add fonts and theme.

---
+++ src/styles/theme.ts
@@ -0,0 +1,104 @@
+export const theme = {
+  colors: {
+    primary: {
+      yellow: "#f1e447",
+      red: "#de001b",
+    },
+    surface: {
+      base: "#160e0b",
+      text1: "#ffffff",
+      text2: "#d0cfce",
+      text3: "#a29f9d",
+      line: "#5c5654",
+    },
+    accent: {
+      gold: "#d0ba03",
+      silver: "#7a7675",
+      bronze: "#3c2617",
+    },
+  },
+  typography: {
+    title: {
+      large: {
+        fontSize: "48px",
+        fontFamily: "'Zen Dots', sans-serif",
+        fontWeight: 400,
+        lineHeight: "56px",
+      },
+      medium: {
+        fontSize: "36px",
+        fontFamily: "'Zen Dots', sans-serif",
+        fontWeight: 400,
+        lineHeight: "40px",
+      },
+      small: {
+        fontSize: "24px",
+        fontFamily: "'Zen Dots', sans-serif",
+        fontWeight: 400,
+        lineHeight: "32px",
+      },
+    },
+    subtitle: {
+      large: {
+        fontSize: "18px",
+        fontFamily: "'Quantico', sans-serif",
+        fontWeight: 700,
+        lineHeight: "24px",
+        fontStyle: "italic",
+        textTransform: "uppercase",
+      },
+      medium: {
+        fontSize: "14px",
+        fontFamily: "'Quantico', sans-serif",
+        fontWeight: 700,
+        lineHeight: "20px",
+        textTransform: "uppercase",
+      },
+    },
+    body: {
+      bold: {
+        fontSize: "14px",
+        fontFamily: "'Quantico', sans-serif",
+        fontWeight: 700,
+        lineHeight: "20px",
+      },
+      regular: {
+        fontSize: "14px",
+        fontFamily: "'Quantico', sans-serif",
+        fontWeight: 400,
+        lineHeight: "20px",
+      },
+      small: {
+        fontSize: "12px",
+        fontFamily: "'Quantico', sans-serif",
+        fontWeight: 400,
+        lineHeight: "16px",
+      },
+    },
+  },
+  border: {
+    width: {
+      thin: "1px",
+      regular: "2px",
+      thick: "4px",
+    },
+    radius: {
+      small: "8px",
+      medium: "16px",
+      large: "32x",
+      round: "999px",
+    },
+  },
+  layout: {
+    space: {
+      xxsmall: "2px",
+      xsmall: "4px",
+      small: "8px",
+      medSmall: "12px",
+      medium: " 16px",
+      medLarge: "24px",
+      large: "32px",
+      xlarge: "64px",
+    },
+  },
+};

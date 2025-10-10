d83c87c12d95db826254138289312ee5156c2407
gcoogan1 <gcoogan1@gmail.com>
2025-10-10 16:27:24 -0400

    Add scroll to top custom hook and add to layout.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../footer/footer"
+import { useScrollToTop } from "../../util/hooks/useScrollToTop"
 
 const Layout = () => {
+  useScrollToTop();
   return (
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <main style={{ flex: 1 }}>
